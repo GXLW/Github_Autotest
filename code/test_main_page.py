7b8d213ff47b4f6f3579c33bd81479bdcba09b9c
GXLW <d3x221@gmail.com>
2021-11-22 00:59:04 +0300

    updating test files

--- code/test_main_page.py
+++ code/test_main_page.py
@@ -2,8 +2,8 @@ from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы - пеходим на страницу логина
-    page.should_be_login_link()      # проверяем
+    page.should_be_login_link()      # проверяем поля логина, регистрации и url
