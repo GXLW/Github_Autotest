7e40ac03951021f0bcf8a3d8ecd0890fac7df20a
GXLW <d3x221@gmail.com>
2021-11-22 01:58:31 +0300

    updating test files

--- code/pages/locators.py
+++ code/pages/locators.py
@@ -1,12 +1,11 @@
 from selenium.webdriver.common.by import By
-from .base_page import BasePage
 
 
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
 
 
-class LoginPageLocators(BasePage):
+class LoginPageLocators:
     def login_form(self):
         self.browser.find_element(By.CSS_SELECTOR, 'form#login_form')
 
